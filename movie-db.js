08b69e73201ad3592b186ad27b6df343332b74ab
cortiz642 <chris.o.ortiz93@gmail.com>
2022-05-12 13:38:48 -0500

    fixed page on load

--- movie-db.js
+++ movie-db.js
@@ -23,42 +23,26 @@
 // When this button is clicked, your javascript should send a DELETE request
 
 
-
-// var delayInMilliseconds = 5000; //1 second
-// $(window).on('load', function () {
-//     $('#loading').hide();
-// })
-// setTimeout(function() {
-//     //your code to be executed after 1 second
-// }, delayInMilliseconds);
-
-
-// var endt = 3000; //20seconds 20 * 1000 milliseconds.
-//
-// function doProgress(){
-//     clearInterval(endt);  //always clear the interval before starting.
-//     $('#spinner').show();
-//     $('#endDiv').hide();
-// }
-
-// function endProgress(){
-//     $('#loopDiv').hide();
-//     $('#endDiv').show();
-//     setInterval(function(){
-//         $('#endDiv').hide();
-//     }, endt);  //hide the div after 20seconds
-// }
-var url = 'https://codeup-json-server.glitch.me/movies'
+var url = 'https://sedate-sharp-euphonium.glitch.me/movies'
 fetch(url).then( response => {
-    response.json().then( users => {
-        users.forEach( userObj  => {
-            // do something with the username login
-            // console.log(userObj.id[255]);
-            if(userObj.title) {console.log(userObj.title)}
-            // if (userObj.id === 16) {console.log(userObj)}
+    response.json().then( movieData => {
+        // movieData.forEach( userObj  => {
+        //     // do something with the username login
+        //     // console.log(userObj.id[255]);
+        //     if(userObj.title) {console.log(userObj.title)}
+        //     if (userObj.id === 16) {console.log(userObj)}
+            movieTitle(movieData)
         });
     });
-});
+// });
 
+function movieTitle(data){
+    data.forEach( element  => {
+    if(element.title) {console.log(element.title)}
+    if (element.id === 16) {console.log(element)}
 
+    // console.log('data.title',data.title);
+
+})
+}
 
